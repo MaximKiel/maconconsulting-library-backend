d98c7a87b7b5d43b6d8e1a74b68343a7714ab7a5
MaximKiel <loup.du.nord@yandex.ru>
2025-06-19 21:22:38 +0300

    Delete lombok annotations from parameters dto

--- src/main/java/ru/maconconsulting/librarybackend/dto/parameters/FormatDTO.java
+++ src/main/java/ru/maconconsulting/librarybackend/dto/parameters/FormatDTO.java
@@ -1,13 +1,4 @@
 package ru.maconconsulting.librarybackend.dto.parameters;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class FormatDTO extends AbstractParameterDTO {
 }
--- src/main/java/ru/maconconsulting/librarybackend/dto/parameters/TypeDTO.java
+++ src/main/java/ru/maconconsulting/librarybackend/dto/parameters/TypeDTO.java
@@ -1,14 +1,5 @@
 package ru.maconconsulting.librarybackend.dto.parameters;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class TypeDTO extends AbstractParameterDTO {
 
 }
